001512e560e57156f39c3b79bfaf67210366061c
Milancodeert <milan.stassen2012@gmail.com>
2025-05-24 10:45:59 +0200

    Les 10

--- startcode/main.py
+++ startcode/main.py
@@ -2,7 +2,8 @@ import pygame
 import time
 from snake import Snake
 from food import Food
-
+from datetime import datetime
+import csv
 # kleuren
 kleur_achtergrond = (0, 0, 0)
 kleur_tekst = (0, 255, 0)
@@ -21,6 +22,40 @@ pygame.init()
 # Creëer een venster met opgegeven breedte en hoogte
 venster = pygame.display.set_mode((breedte, hoogte))
 pygame.display.set_caption('Snake')
+def toon_tijd(tijd, venster):
+    tijd = pygame.time.get_ticks()
+    font = pygame.font.Font(None, 36)
+    Tijd_tekst = font.render(f"Tijd gespeeld: {tijd}", True, kleur_tekst)
+    venster.blit(Tijd_tekst, (0, 10))
+
+
+
+def sla_op_in_CSV(score, tijdstip):
+    with open("highscores.csv", mode="a", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([score, tijdstip])
+
+
+def haal_hoogste_score_op():
+    try:  # zorgt er voor dat als het bestand niet bestaat, we niet crashen
+        with open('highscores.csv', mode='r') as file:  # open het bestand en sla het op in de file variabele
+
+            reader = csv.reader(file)  # maak er een csv-lezer van
+
+            hoogste_score = list(reader)  # vorm die lezer om naar een 2-dimensionale lijst (= een lijst met lijsten)
+            maximum_score = max(
+                int(row[0]) for row in hoogste_score)  # Zoek het maximum van alle tweede waardes (de leeftijden)
+            return maximum_score
+
+    except FileNotFoundError:  # als de file niet gevonden wordt gewoon rustig blijven ;)
+        return 0
+
+    hoogstescore = max(int(row[0]) for row in highscores)
+
+
+
+
+
 
 # Functie om de score op het scherm te tonen
 def toon_score(score, venster):
@@ -52,6 +87,20 @@ def game_lus():
                 elif event.key == pygame.K_DOWN and snake.y_verandering == 0:
                     snake.y_verandering = veld_grootte
                     snake.x_verandering = 0
+                elif event.key == pygame.K_p:
+                    gepauzeerd = True
+                    pauze_font = pygame.font.Font(None, 36)
+                    pauze_tekst = pauze_font.render("Pauze (Druk op P om door te gaan)", True, kleur_tekst)
+                    venster.blit(pauze_tekst, (breedte //2 -
+                                               pauze_tekst.get_width() // 2,
+                                               hoogte // 2))
+                    pygame.display.update()
+                    while gepauzeerd:
+                        for pauze_event in pygame.event.get():
+                            if pauze_event.type == pygame.KEYDOWN and pauze_event.key == pygame.K_p:
+                                gepauzeerd = False
+
+
 
         snake.beweeg()
         if snake.is_buiten_veld(breedte, hoogte) or snake.raakt_zichzelf():
@@ -71,6 +120,12 @@ def game_lus():
         time.sleep(1 / spel_snelheid)
 
     print(f"Jouw score is {score}")
+    sla_op_in_CSV(score,datetime.now())
+    hoogste_score = haal_hoogste_score_op()
+    print(f"De hoogste score is {hoogste_score}")
+
+
+
 
 # Start de hoofdloop van het spel
 game_lus()
--- startcode/snake.py
+++ startcode/snake.py
@@ -1,7 +1,9 @@
 import pygame
 
-kleur_slang = (0, 255, 0)
-veld_grootte = 20
+kleur_slang1 = (0, 255, 0)
+kleur_slang2 = (0, 200, 0)
+veld_grootte1 = 20
+veld_grootte2 = 12
 
 class Snake:
     def __init__(self, startx, starty):
@@ -23,7 +25,8 @@ class Snake:
 
     def teken(self, venster):
         for segment in self.lijst_slang:
-            pygame.draw.rect(venster, kleur_slang, pygame.Rect(segment[0], segment[1], veld_grootte, veld_grootte))
+            pygame.draw.rect(venster, kleur_slang1, pygame.Rect(segment[0], segment[1], veld_grootte1, veld_grootte1))
+            pygame.draw.rect(venster, kleur_slang2, pygame.Rect(segment[0] + 4, segment[1] + 4, veld_grootte2, veld_grootte2))
 
     def is_buiten_veld(self, breedte, hoogte):
         return self.x >= breedte or self.x < 0 or self.y >= hoogte or self.y < 0
